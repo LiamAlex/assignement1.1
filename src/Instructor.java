ede08f16f66a05af6338e7cd435bbf4c6481b976
daley <daley@10.250.15.191>
2018-03-19 19:17:42 -0400

    initial upload

---
+++ src/Instructor.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.time.LocalDate;
+import java.time.Month;
+/**
+ *
+ * @author daley
+ */
+public class Instructor {
+    private String firstName;
+    private String lastName;
+    private String strretAddress;
+    private String city;
+    private String postalCode;
+    private LocalDate hireDate;
+    private LocalDate birthday;
+    private int employeeNum;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getStrretAddress() {
+        return strretAddress;
+    }
+
+    public void setStrretAddress(String strretAddress) {
+        this.strretAddress = strretAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public LocalDate getHireDate() {
+        return hireDate;
+    }
+
+    public void setHireDate(LocalDate hireDate) {
+        this.hireDate = hireDate;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getEmployeeNum() {
+        return employeeNum;
+    }
+
+    public void setEmployeeNum(int employeeNum) {
+        this.employeeNum = employeeNum;
+    }
+    public void getYearBorn(LocalDate birthday){
+     return birthday.getYear();
+    }
+    
+    public void
+    
+}
