af8c2005d40684e425fb05fcafd9788f9f5f5831
daley <daley@10.250.15.191>
2018-03-19 19:17:20 -0400

    initial upload

---
+++ src/Course.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.time.LocalDate;
+import java.time.Month;
+/**
+ *
+ * @author daley
+ */
+public class Course extends Instructor{
+    private Instructor instructor= new Instructor();
+    private String courseCode;
+    private String courseName;
+    private String room;
+    private int maxStudents;
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public int getMaxStudents() {
+        return maxStudents;
+    }
+
+    public void setMaxStudents(int maxStudents) {
+        this.maxStudents = maxStudents;
+    }
+    
+    
+}
